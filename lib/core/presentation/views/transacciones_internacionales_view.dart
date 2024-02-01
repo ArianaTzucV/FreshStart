988607fd6926e84cec012db0d321d1f14bc2067b
Wirbink <irvingramoncoyoltoliveros@gmail.com>
2024-02-01 01:06:22 -0500

    new screen TransIntl

--- lib/core/presentation/views/transacciones_internacionales_view.dart
+++ lib/core/presentation/views/transacciones_internacionales_view.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:fresh_start/core/presentation/widgets/add_button.dart';
+import 'package:fresh_start/core/presentation/widgets/general_button.dart';
+import 'package:fresh_start/main.dart';
 
 class TransaccionesIntl extends StatelessWidget {
-  const TransaccionesIntl({super.key});
+  const TransaccionesIntl({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Transacciones Internacionales"),
       ),
-      body: Container(
-        child: const Column(
-          children: [
-            SizedBox(height: 20.0),
-            AddButtonWidget(),
-            Text("Beneficiarios Guardados")
-          ],
-        ),
+      body: const Column(
+        children: [
+          SizedBox(height: 20.0),
+          Row(
+            children: [
+              AddButtonWidget(),
+              SizedBox(
+                  width: 10), // Puedes ajustar este espacio según sea necesario
+              Text(
+                'Inscribir nuevo beneficiario',
+                style: TextStyle(
+                  color: Color(0xFF1A2D4D), // Puedes ajustar el color del texto
+                  fontSize: 16.0, // Puedes ajustar el tamaño del texto
+                ),
+              ),
+            ],
+          ),
+          Text("Beneficiarios Guardados"),
+          GeneralButtonWidget(
+            text: "Continuar"
+          )
+        ],
       ),
     );
   }
---
+++ lib/core/presentation/widgets/general_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GeneralButtonWidget extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+
+  const GeneralButtonWidget({Key? key, required this.text, this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.8,
+        child: SizedBox(
+          height: 35.0, // Altura fija de 50 píxeles
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: Text(text),
+          ),
+        ),
+      ),
+    );
+  }
+}
