b04950fd44e61f1daedb8eb015249ce76a31b228
Aldair Coyolt <aldaircoyotl@gmail.com>
2024-02-11 17:00:12 -0500

    Bugfix

--- lib/core/presentation/widgets/home/service_icon_button.dart
+++ lib/core/presentation/widgets/home/service_icon_button.dart
@@ -6,6 +6,7 @@ class ServiceIconButton extends StatelessWidget {
   final Icon icon;
   final Color bgColor;
   final Color fgColor;
+  final void Function() onTap;
 
   const ServiceIconButton({
     super.key,
@@ -14,38 +15,42 @@ class ServiceIconButton extends StatelessWidget {
     required this.icon,
     required this.bgColor,
     required this.fgColor,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CircleAvatar(
-          backgroundColor: bgColor,
-          radius: 25.0,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [icon],
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundColor: bgColor,
+            radius: 25.0,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [icon],
+            ),
           ),
-        ),
-        Text(
-          title1,
-          style: TextStyle(
-              fontSize: 12.0,
-              fontFamily: 'MarkPro',
-              fontWeight: FontWeight.normal,
-              color: fgColor),
-        ),
-        Text(
-          title2,
-          style: TextStyle(
-              fontSize: 12.0,
-              fontFamily: 'MarkPro',
-              fontWeight: FontWeight.normal,
-              color: fgColor),
-        )
-      ],
+          Text(
+            title1,
+            style: TextStyle(
+                fontSize: 12.0,
+                fontFamily: 'MarkPro',
+                fontWeight: FontWeight.normal,
+                color: fgColor),
+          ),
+          Text(
+            title2,
+            style: TextStyle(
+                fontSize: 12.0,
+                fontFamily: 'MarkPro',
+                fontWeight: FontWeight.normal,
+                color: fgColor),
+          )
+        ],
+      ),
     );
   }
 }
