1536259393b8f2f07eb690cb8e62b8d363692393
Domingo Ovando <goroleta0nuevo@gmail.com>
2024-04-08 17:26:28 -0600

    Modified

--- lib/core/presentation/views/Services Payment/services_payment_view.dart
+++ lib/core/presentation/views/Services Payment/services_payment_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(EstadoDeCuentaWidget());
-// }
+void main() {
+  runApp(EstadoDeCuentaWidget());
+}
 
 class EstadoDeCuentaWidget extends StatelessWidget {
   @override
