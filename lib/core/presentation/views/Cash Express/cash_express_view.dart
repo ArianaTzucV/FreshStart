8893ba60a30a0f0be22a63e974885ce58c32ca41
Brayn Sahagun <p.brayn.sahagun@go1.mx>
2024-04-10 12:09:37 -0500

    add confirm

--- lib/core/presentation/views/Cash Express/cash_express_view.dart
+++ lib/core/presentation/views/Cash Express/cash_express_view.dart
@@ -146,27 +146,333 @@ class CashExpress extends StatelessWidget {
                   )
                 ],
               ),
-              Expanded(
-                child: Container(),
-              ),
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AddNewContactView()));
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.only(bottom: 25.0),
-                    child: const GeneralButtonWidget(text: 'Continuar'),
+              const SizedBox(height: 200),
+              ElevatedButton(
+                onPressed: () {
+                  _showBottomSheet(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF1A2D4D),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 120.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
+                ),
+                child: const Text(
+                  "Continuar",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'MarkPro'),
                 ),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          padding: const EdgeInsets.all(30.0),
+          decoration: const BoxDecoration(
+              color: Color(0xFFFAFAFA),
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Retiro Disponible",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'MarkPro',
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const CircleAvatar(
+                backgroundColor: Color(0xFF20B087),
+                radius: 40,
+                child: Icon(
+                  Icons.check,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Pagó",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "Brayn Sahagun",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Cuenta",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                  Text(
+                    "5558 •••• •••• ••84",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Clabe Destino",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "123456789012345678",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Fecha",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "17 Abril, 2024",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "7:08 pm",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Clave de Retiro",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "5533 4387 2123",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "*12 dígitos",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Código de seguridad",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "1512",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "*4 dígitos",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Monto",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                  Text(
+                    "\$32,524",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Roboto',
+                      color: Color(0xFF060912),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Puedes compartir esta clave para que otra persona haga el retiro.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'MarkPro',
+                      color: Color(0xFF606167),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF1A2D4D),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 18.0, horizontal: 145.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                child: const Text(
+                  "Salir",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'MarkPro'),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
