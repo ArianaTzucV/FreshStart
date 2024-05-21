76f86d4232bad4a18fd36065d590f387690bcc4c
Jesus Quiroga <202100176@upqroo.edu.mx>
2024-05-21 17:53:10 -0500

    Add Json_data and models of CashExpress

---
+++ lib/domain/models/amount_cash_express_model.dart
@@ -0,0 +1,23 @@
+class amountcash{
+String pay;
+String account;
+int  clabeDestination;
+String date;
+String hour; 
+String withdrawalKey;
+int securityCode;
+int amount;
+
+
+amountcash({
+
+  required this.pay,
+  required this.account, 
+  required this.clabeDestination,
+  required this.date,
+  required this.hour,
+  required this.withdrawalKey,
+  required this.securityCode,
+  required this.amount
+});
+}
