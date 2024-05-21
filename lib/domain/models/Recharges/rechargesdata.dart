54c8f175691b946d84bdb8d7a317224f47e13cf8
BrianJ <202100080@upqroo.edu.mx>
2024-05-21 11:23:08 -0500

    json_and_lib_modification

---
+++ lib/domain/models/Recharges/rechargesdata.dart
@@ -0,0 +1,14 @@
+class RechargeModel {
+  int phone_number;
+  String pay_with;
+  String recharge_type;
+  double amount;
+
+  RechargeModel({
+    required this.phone_number,
+    required this.pay_with,
+    required this.recharge_type,
+    required this.amount,
+  });
+}
+
