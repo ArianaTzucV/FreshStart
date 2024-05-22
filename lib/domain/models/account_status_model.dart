1d8caa90ed473374d260a1034104927c69f60170
BryanH <202100079@upqroo.edu.mx>
2024-05-22 17:34:24 -0500

    Cambiosxd

--- lib/domain/models/account_status_model.dart
+++ lib/domain/models/account_status_model.dart
@@ -1,5 +1,9 @@
 import 'dart:ffi';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'form_model.g.dart';
+
+@JsonSerializable()
 class AccountStatusModel {
   String myAccount;
   Double availableBalance;
@@ -8,4 +12,8 @@ class AccountStatusModel {
     required this.myAccount,
     required this.availableBalance
   });
+
+  factory AccountStatusModel.fromJson(Map<String, dynamic> json) => _$AccountStatusModel(json);
+
+  Map<String, dynamic> toJson() => _$AccountStatusModel(this);
 }
